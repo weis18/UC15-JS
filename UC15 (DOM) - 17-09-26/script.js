b258c523347ae6145d559bdf16b78d719a4b2cda
weis18 <wcauarodmoura@gmail.com>
2026-09-17 10:24:01 -0300

    Add files via upload

---
+++ UC15 (DOM) - 17-09-26/script.js
@@ -0,0 +1,9 @@
+function mudarTexto() {
+    document.getElementById
+    ("titulo").textContent = "Você clicou no botão!";
+}
+
+function mudarCor() {
+    document.getElementById
+    ("mensagem").style.color = "blue";
+}
